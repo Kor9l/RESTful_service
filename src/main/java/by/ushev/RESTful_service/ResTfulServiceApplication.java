a66d9229adf37cdaa3478f488d99bd2f14b7b70f
Kor9l <korianko@gmail.com>
2021-09-28 19:23:00 +0300

    add dependencies

---
+++ src/main/java/by/ushev/RESTful_service/ResTfulServiceApplication.java
@@ -0,0 +1,13 @@
+package by.ushev.RESTful_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulServiceApplication.class, args);
+	}
+
+}
